7413d6f980efc80d6632050af06927ca6a563adc
HolMoly <ryan10920@naver.com>
2024-09-10 06:26:35 +0900

    refactor: api & repository 통합

--- src/infrastructure/http/api/interactionApi.js
+++ src/infrastructure/http/api/interactionApi.js
@@ -1,5 +1,10 @@
 import httpClient from '../httpClient'
 
+// 문의 작성
+export const postInquiry = async (body) => {
+  const response = await httpClient.post(`/interaction/inquiry`, body);
+  return response;
+}
 // 피드백 작성
 export const postFeedback = async (body) => {
   const response = await httpClient.post('/interaction/feedback', body)
--- src/infrastructure/repositories/InteractionRepository.js
+++ src/infrastructure/repositories/InteractionRepository.js
@@ -1,10 +1,14 @@
-import { postFeedback } from '../http/api/interactionApi'
+import { postInquiry ,postFeedback } from '../http/api/interactionApi'
 
 export class InteractionRepository {
+  // 문의 전송
+  async postInquiry(body) {
+    const response = await postInquiry(body)
+    return response
+  }
   // 피드백 전송
   async postFeedback(body) {
     const response = await postFeedback(body)
-
     return response
   }
 }
