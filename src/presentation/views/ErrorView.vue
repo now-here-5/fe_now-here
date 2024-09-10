013732bc29c1f055d5a4c8f7bf83afa56db5f7c5
HolMoly <ryan10920@naver.com>
2024-09-11 02:59:16 +0900

    Refactor: Error 정리

--- src/presentation/views/ErrorView.vue
+++ src/presentation/views/ErrorView.vue
@@ -1,38 +1,30 @@
 <template>
-  <div class="Frame">
+  <div class="frame">
     <img class="imageContainer" src="/images/404_error.png" alt="error" />
-    <p v-html="'페이지에 접근할 수 없습니다.<br> QR코드를 통해 접속해주세요'"></p>
+    <span v-html="'페이지에 접근할 수 없습니다.<br> QR코드를 통해 접속해주세요'"></span>
   </div>
 </template>
 
-<script setup></script>
+<script setup />
 
 <style scoped lang="scss">
-.Frame {
+.frame {
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  padding: 150px 0px;
+  padding: 150px 0;
   gap: 50px;
-
   height: 100vh;
-  p {
+  span {
     font-weight: $textS_weight;
     font-size: $textM_size;
     text-align: center;
-
     color: $dark;
   }
 }
 .imageContainer {
   width: 300px;
   height: 215px;
-}
-/* 데스크톱에 적용할 스타일 */
-@media only screen and (min-width: 601px) {
-  .Frame {
-    justify-content: center;
-  }
 }
 </style>
