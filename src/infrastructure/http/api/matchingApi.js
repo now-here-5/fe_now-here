d01b18ca4d266e026a6527fb68ba78638315ffe4
HolMoly <ryan10920@naver.com>
2024-09-11 12:24:13 +0900

    Refactor: matchingApi 반영

--- src/infrastructure/http/api/matchingApi.js
+++ src/infrastructure/http/api/matchingApi.js
@@ -28,3 +28,33 @@ export const getMatchingNotificationList = async () => {
   const { data } = await httpClient.get('/matching/getNotificationList')
   return data
 }
+
+// 하트 보내기
+export const postSendHeart = async (receiverId) => {
+  const { data } = await httpClient.post(`/matching/send/${receiverId}`)
+  return data
+}
+
+// 받은 하트 페이지 조회
+export const getReceivedHeartList = async () => {
+  const { data } = await httpClient.get(`/matching/senderList`)
+  return data
+}
+
+// 하트 수락하기
+export const postReceivedHeart = async (senderId) => {
+  const { data } = await httpClient.patch(`/matching/receive/${senderId}`)
+  return data
+}
+
+// 보낸 하트 페이지 조회
+export const getSentHeartList = async () => {
+  const { data } = await httpClient.get(`/matching/receiverList`)
+  return data
+}
+
+// 매칭 현황 페이지 조회
+export const getMatchStatusList = async () => {
+  const { data } = await httpClient.get(`/matching/summaryDetail`)
+  return data
+}
