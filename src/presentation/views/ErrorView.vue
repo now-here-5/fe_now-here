2e9ad05f2a6da0ee90f50edffe3e6cdf4ac31540
HolMoly <ryan10920@naver.com>
2024-09-11 12:24:42 +0900

    Refactor: ErrorView permission 추가

--- src/presentation/views/ErrorView.vue
+++ src/presentation/views/ErrorView.vue
@@ -2,10 +2,13 @@
   <div class="frame">
     <img class="imageContainer" src="/images/404_error.png" alt="error" />
     <span v-html="'페이지에 접근할 수 없습니다.<br> QR코드를 통해 접속해주세요'"></span>
+    <button @click="requestPermission">allow</button>
   </div>
 </template>
 
-<script setup />
+<script setup>
+import { requestPermission } from '@/composables/useNotification'
+</script>
 
 <style scoped lang="scss">
 .frame {
