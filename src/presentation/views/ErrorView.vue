0ac610cdd326b28213d48f348c530b130f06c8b5
HolMoly <ryan10920@naver.com>
2024-09-12 17:26:32 +0900

    Refactor: errorView 수정

--- src/presentation/views/ErrorView.vue
+++ src/presentation/views/ErrorView.vue
@@ -2,12 +2,15 @@
   <div class="frame">
     <img class="imageContainer" src="/images/404_error.png" alt="error" />
     <span v-html="'페이지에 접근할 수 없습니다.<br> QR코드를 통해 접속해주세요'"></span>
-    <button @click="requestPermission">allow</button>
   </div>
 </template>
 
 <script setup>
-import { requestPermission } from '@/composables/useNotification'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  alert('비정상적인 접근입니다')
+})
 </script>
 
 <style scoped lang="scss">
