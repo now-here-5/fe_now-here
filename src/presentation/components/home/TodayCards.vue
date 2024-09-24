3a4e7401f736249be9a52dc11ef6b9ddda5dfded
여인수 <insu12021202@gmail.com>
2024-09-24 22:06:16 +0900

    Fix: #27 - 홈 > 매칭 가능한 상대 없을 시 문구 노출

--- src/presentation/components/home/TodayCards.vue
+++ src/presentation/components/home/TodayCards.vue
@@ -3,12 +3,18 @@
     <span class="title">오늘의 카드</span>
     <span class="desc">카드를 누르면 더 많은 정보를 확인할 수 있어요!</span>
     <div class="cards-wrapper" @click="moveToMatch">
-      <div v-if="!recommendedMembers.length" class="loading-wrapper">
+      <div v-if="isLoading" class="loading-wrapper">
         <LoadingSpinner />
       </div>
       <template v-else>
-        <TodayCardItem :member-info="recommendedMembers[0]" />
-        <TodayCardItem :member-info="recommendedMembers[1]" />
+        <template v-if="recommendedMembers.length > 0">
+          <TodayCardItem :member-info="recommendedMembers[0]" />
+          <TodayCardItem :member-info="recommendedMembers[1]" />
+        </template>
+        <div v-else class="no-match-wrapper">
+          <span class="title">현재 매칭 가능한 상대가 없습니다.</span>
+          <span>조금만 기다려주세요!</span>
+        </div>
       </template>
     </div>
   </div>
@@ -18,20 +24,23 @@
 import TodayCardItem from '@/presentation/components/home/TodayCardItem.vue'
 import { useMatchingStore } from '@/presentation/stores/matchingStore'
 import { storeToRefs } from 'pinia'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import LoadingSpinner from '../LoadingSpinner.vue'
 import { useRouter } from 'vue-router'
 
 const matchingStore = useMatchingStore()
 const { recommendedMembers } = storeToRefs(matchingStore)
 const router = useRouter()
+const isLoading = ref(false)
 
 const moveToMatch = () => {
   router.push('/match')
 }
 
 onMounted(async () => {
+  isLoading.value = true
   await matchingStore.fetchRecommendedCards()
+  isLoading.value = false
 })
 </script>
 
@@ -61,6 +70,20 @@ onMounted(async () => {
       align-items: center;
       width: 100%;
       height: 230px;
+    }
+
+    .no-match-wrapper {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      width: 100%;
+      height: 230px;
+
+      .title {
+        font-size: $textXL_size;
+        font-weight: $textB_weight;
+      }
     }
   }
 }
