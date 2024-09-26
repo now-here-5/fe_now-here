eb4370a73b4a80998e579fa95c3d9c80b74d6649
여인수 <insu12021202@gmail.com>
2024-09-26 21:45:21 +0900

    Fix: #31 - API endpoint 수정

--- src/infrastructure/http/api/adminEventApi.js
+++ src/infrastructure/http/api/adminEventApi.js
@@ -1,6 +1,6 @@
 import httpClient from '@/infrastructure/http//httpClient'
 
 export const getEventList = async () => {
-	const response = await httpClient.get('/admin/event/list?status=true');
-	return response.data.data;
+  const response = await httpClient.get('/event/list?status=true')
+  return response.data.data
 }
