63bc7cf72d1b56eb36e0b280e2e7478da0e25d2b
여인수 <insu12021202@gmail.com>
2024-09-29 19:34:08 +0900

    Feat :g-tag 적용

--- src/main.js
+++ src/main.js
@@ -2,17 +2,28 @@ import '@/assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import piniaPersist from 'pinia-plugin-persistedstate'; // pinia persist 추가
+import piniaPersist from 'pinia-plugin-persistedstate' // pinia persist 추가
 
 import App from '@/App.vue'
 import router from '@/presentation/router'
+import VueGtag from 'vue-gtag-next'
 
 const app = createApp(App)
 
-const pinia = createPinia();
-pinia.use(piniaPersist); // Pinia에 persist 추가
+const pinia = createPinia()
+pinia.use(piniaPersist) // Pinia에 persist 추가
 
 app.use(pinia) // 이미 생성된 pinia 사용
 app.use(router)
+
+app.use(
+  VueGtag,
+  {
+    property: {
+      id: 'G-KMRVZ0WBRB'
+    }
+  },
+  router
+)
 
 app.mount('#app')
